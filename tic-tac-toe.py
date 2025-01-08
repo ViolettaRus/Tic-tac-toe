e104cf4418a293bdb17ebd0a9e7ffc2f6c04e278
ViolettaRus <67428352+ViolettaRus@users.noreply.github.com>
2025-01-08 13:53:02 +0300

    Add files via upload

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,5 +1,6 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 # Параметры обучения
 alpha = 0.1  # скорость обучения
@@ -20,48 +21,80 @@ def get_state(board):
     return state
 
 # Функция для выбора действия
-def choose_action(state, epsilon):
-    if random.random() < epsilon:
-        return random.randint(0, 8)
+def choose_action(state, epsilon, Q, board):
+    possible_actions = [i for i, val in enumerate(board) if val == ' ']
+    if random.random() < epsilon:  
+        return random.choice(possible_actions)
     else:
         return np.argmax(Q[state])
 
 # Функция для обновления Q-значений
-def update_Q(state, action, reward, next_state):
-    Q[state, action] = Q[state, action] + alpha * (reward + gamma * np.max(Q[next_state]) - Q[state, action])
+def update_Q(state, action, reward, next_state, Q):
+    if next_state is None:
+      Q[state, action] = Q[state, action] + alpha * (reward - Q[state, action])
+    else:
+        max_next_q = max(Q[next_state, a] for a in range(9))
+        Q[state, action] = Q[state, action] + alpha * (reward + gamma * max_next_q - Q[state, action])
+
 
 # Функция для игры
 def play_game(agent, opponent):
     board = [' '] * 9
     current_player = 'X'
-    while not check_win(board, current_player) and ' ' in board:
-        if current_player == 'X':
-            state = get_state(board)
-            action = agent.choose_action(state, epsilon)
-            board[action] = 'X'
-        else:
+    state = get_state(board)
+    while True:
+        if current_player == 'X':  # Ход агента
+            action = agent.choose_action(state, epsilon, Q, board)
+        else:  # Ход оппонента
             action = opponent.choose_action(board)
-            board[action] = 'O'
+
+        if board[action] != ' ':
+            # Недопустимый ход
+            reward = -1 if current_player == 'X' else 0
+            next_state = None
+            update_Q(state, action, reward, next_state, Q)
+            return reward
+
+        board[action] = current_player
+        winner = check_win(board)
+
+        if winner:  # Если есть победитель
+            reward = 1 if winner == 'X' else -1
+            next_state = None
+            update_Q(state, action, reward, next_state, Q)
+            return reward
+
+        if ' ' not in board:  # Ничья
+            reward = 0.5
+            next_state = None
+            update_Q(state, action, reward, next_state, Q)
+            return reward
+
+        next_state = get_state(board)
+        if current_player == 'X':  # Обновляем Q для агента
+            update_Q(state, action, 0, next_state, Q)
+
+        state = next_state
         current_player = 'O' if current_player == 'X' else 'X'
 
-    # Награждение агента
-    reward = 1 if check_win(board, 'X') else -1 if check_win(board, 'O') else 0
-    return reward
+    return 0
+
 
 # Функция для проверки победы
-def check_win(board, player):
+def check_win(board):
     win_combinations = [(0, 1, 2), (3, 4, 5), (6, 7, 8),
                        (0, 3, 6), (1, 4, 7), (2, 5, 8),
                        (0, 4, 8), (2, 4, 6)]
     for combination in win_combinations:
-        if board[combination[0]] == board[combination[1]] == board[combination[2]] == player:
-            return True
-    return False
+        if board[combination[0]] == board[combination[1]] == board[combination[2]] != ' ':
+            return board[combination[0]]
+    return None
+
 
 # Определение агента и оппонента
 class Agent:
-    def choose_action(self, state, epsilon):
-        return choose_action(state, epsilon)
+    def choose_action(self, state, epsilon, Q, board):
+        return choose_action(state, epsilon, Q, board)
 
 class RandomOpponent:
     def choose_action(self, board):
@@ -74,16 +107,17 @@ opponent = RandomOpponent()
 
 # Обучение агента
 rewards = []
-for i in range(10000):
+for i in range(20000):
     reward = play_game(agent, opponent)
     rewards.append(reward)
 
-# Построение кривой обучения
-average_rewards = [np.mean(rewards[i-100:i]) for i in range(100, len(rewards))]
+# Построение кривой обучения (скользящее среднее)
+window_size = 100
+average_rewards = np.convolve(rewards, np.ones(window_size), 'valid') / window_size
 
 # Построение графика
-import matplotlib.pyplot as plt
 plt.plot(average_rewards)
 plt.xlabel("Количество шагов обучения")
 plt.ylabel("Средняя награда")
+plt.title("Кривая обучения агента")
 plt.show()
